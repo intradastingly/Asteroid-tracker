dd1185a4c32a63a2be4b0c47bf8c855742e075b5
Intradastingly <asaf_lackgren@hotmail.com>
2021-02-23 13:06:45 +0100

    Co-authored-by: lilgujj <gustavkarlandersson@hotmail.com>

---
+++ src/components/Header.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Header() {
+    return (
+        <div>
+            <h1>ASTEROID TRACKER</h1>
+        </div>
+    )
+}
+
+export default Header;    )
+}
+
+    )
+}
+
+export default Header;
