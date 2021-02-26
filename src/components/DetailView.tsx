9232793ad0117b70bbeb36d623a7e5b95d42508b
Camilla Johansson <camilla.johansson1986@gmail.com>
2021-02-26 13:44:39 +0100

    Co-authored-by: Anton Mäenpää <antonmaenpaa@gmail.com> Co-authored-by: lilgujj <gustavkarlandersson@hotmail.com>

---
+++ src/components/DetailView.tsx
@@ -0,0 +1,51 @@
+
+import React, { CSSProperties } from "react";
+import { Link } from "react-router-dom";
+import Buttons from './buttons';
+
+
+interface Props {
+    image: string,
+    drinkTitle: string,
+    drinkRecipe: string,
+}
+
+function DetailView(props: Props) {
+
+function somethingHappens(){
+    //route back to master view
+}
+    
+
+    return (
+        <div style={rootStyle}>
+            <img style={imageStyle} src={props.image} alt=""/>
+             <h2 >{props.drinkTitle}</h2>
+             <p>{props.drinkRecipe}</p>
+             <Link to="/">
+                 <Buttons text="Back" handleClick={somethingHappens}/>
+             </Link>
+        </div>
+    );
+
+}
+
+const rootStyle: CSSProperties = {
+    display: 'flex',
+    position: 'relative',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'column',
+    flex: "1",
+}
+
+const imageStyle: CSSProperties = {
+    maxWidth: "10rem",
+    borderRadius: "2rem",
+    margin: "2rem 0",
+}
+
+
+
+
+export default DetailView;
