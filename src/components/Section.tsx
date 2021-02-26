54125ad8ff53aef6e1e0fce2ae92735e43dbd53c
FIRST_GUSTAV LAST_ANDERSSON <Gustavkarlandersson@hotmail.com>
2021-02-26 08:56:49 +0100

    yes

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -30,6 +30,7 @@ const imageStyle: CSSProperties = {
     objectFit: "cover",
     width: "100%",
     boxShadow: "0px 0px 10px 0px rgba(0, 0, 0, 0.75)",
+    borderRadius: "1rem",
 }
 
 const textStyle: CSSProperties = {
