fbdbe08afe9ce83111d2e111256c97e1f28ee4d0
Intradastingly <asaf_lackgren@hotmail.com>
2021-03-02 12:27:19 +0100

    drink

--- src/components/MasterView.tsx
+++ src/components/MasterView.tsx
@@ -8,100 +8,51 @@ interface Props extends RouteComponentProps {
     onSearchDrink: (value: []) => void;
 }
 
-
 interface State {
     searchValue: string;
-    // data: Drinks[];
+
 }
 class MasterView extends Component<Props, State> {
     state: State = {
-        searchValue: '',
-        // data: []
+        searchValue: ''
     }
 
     COCKTAILS_URL = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
-    RANDOM_URL = "https://www.thecocktaildb.com/api/json/v1/1/random.php";
-    
-    // async fetchDataFromApi(searchValue: string) {
-    //     try {
-    //         fetch(this.COCKTAILS_URL + searchValue)
-    //             .then(response => response.json())
-    //             .then(drink => {
-    //                 this.props.onSearchDrink(drink);
-    //                 this.props.history.push('/search')
-    //             })
-        
-    //     } catch (error) {
-    //         console.log(error);
-    //     }
-    // } 
-
-    
-  async fetchImagesFromApi(searchValue: string) {
-    try {
-      const url = this.COCKTAILS_URL + searchValue;
-      
-      const response = await fetch(url);
-      
-      const result = await response.json();
-      console.log(result.drinks)
-      this.props.onSearchDrink(result.drinks);
-      //this.setState({ data: result.drinks[0] })
-    } catch (error: unknown) {
-      console.error(error);
+     
+    async fetchImagesFromApi(searchValue: string) {
+        try {
+            const url = this.COCKTAILS_URL + searchValue;
+            const response = await fetch(url);
+            const result = await response.json();
+            this.props.onSearchDrink(result.drinks);
+        } catch (error: unknown) {
+            console.error(error);
+        }
     }
-  }
-
-    // Generates random drink when random button is clicked
-
-    // getRandomCocktail() {
-    //     fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php')
-    //     .then(
-    //         (response) => {
-    //             if (response.status !== 200) {
-    //                 console.log('Looks like there was a problem. Status Code: ' +
-    //                 response.status);
-    //                 return;
-    //             }
-    //             response.json().then((data) => {
-    //                 this.props.randomDrink(data);
-    //                 this.props.history.push('/Random')       
-    //                 // console.log(data.drinks[0].strDrink);
-                    
-    //                 // console.log(data.drinks);
-    //             })
-    //         }
-    //     )
-    //     .catch(function(err) {
-    //         console.log('Fetch Error : S' , err)
-    //     })
-    // }
 
     somethingHappens = () => {
         this.fetchImagesFromApi(this.state.searchValue)
-        // this.fetchDataFromApi(this.state.searchValue);
-        // console.log(drink.drinks[0].strDrink);
     }
 
     handleSearchResult = (value: string) => {
+        this.fetchImagesFromApi(this.state.searchValue)
         this.setState({ searchValue: value });
     }
 
     render() {
-
+        console.log(this.props.drink)
         return (
             <div style={rootStyle}>
                 <StartPageImage />
-                <SearchBar value={this.state.searchValue} onChange={this.handleSearchResult}/>
+                <SearchBar 
+                    value={this.state.searchValue} 
+                    onChange={this.handleSearchResult}
+                    
+                />
                 <div style={buttonFlex}>
                     <Link to="/search">
                         <Buttons text="Search" handleClick={this.somethingHappens}/>
                     </Link>
-
-                    {/* <Link to="/Random">
-                        <Buttons text="Random" handleClick={this.getRandomCocktail}/>
-                    </Link> */}
-                    
                 </div>
             </div>
         )  
