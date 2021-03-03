50bc561c68ddbe24d376cf6331d19c6aa0d32aa4
Intradastingly <asaf_lackgren@hotmail.com>
2021-03-03 15:47:10 +0100

    styling betterish

--- src/components/DetailView.tsx
+++ src/components/DetailView.tsx
@@ -23,7 +23,7 @@ function DetailView(props: Props) {
     return (
         <div style={rootStyle}>
             <div>
-                <h2 >{props.drinkTitle}</h2>
+                <h2 style={h2Style}>{props.drinkTitle}</h2>
             </div>
                 <img style={imageStyle} src={props.image} alt="" />
             <div style={innerDivStyle}>
@@ -33,11 +33,11 @@ function DetailView(props: Props) {
                 </div>
                 <div>
                     {/* <h4>Ingredients</h4> */}
-                        <p style={pStyling}>{props.ingredients1} {props.ingredientsAmount}</p>
-                        <p style={pStyling}>{props.ingredients2} {props.ingredientsAmount1}</p>
-                        <p style={pStyling}>{props.ingredients3} {props.ingredientsAmount2}</p>
-                        <p style={pStyling}>{props.ingredients4} {props.ingredientsAmount3}</p>
-                        <p style={pStyling}>{props.ingredients5} {props.ingredientsAmount4}</p> 
+                        <p style={pStyling}>{props.ingredients1}  {props.ingredientsAmount}</p>
+                        <p style={pStyling}>{props.ingredients2}  {props.ingredientsAmount1}</p>
+                        <p style={pStyling}>{props.ingredients3}  {props.ingredientsAmount2}</p>
+                        <p style={pStyling}>{props.ingredients4}  {props.ingredientsAmount3}</p>
+                        <p style={pStyling}>{props.ingredients5}  {props.ingredientsAmount4}</p> 
                 </div>
             </div>
         </div>    
@@ -50,28 +50,25 @@ const rootStyle: CSSProperties = {
     justifyContent: 'center',
     alignItems: 'center',
     flexDirection: 'column',
-    width: "12rem",
+    width: "20rem",
     textAlign: 'center',
     margin: '1rem',
-    backgroundColor: '#6d0000',
-    padding: '2rem',
+    backgroundColor: '#ffeed1',
+    padding: '1rem',
     borderRadius: '2rem',
-    color: "white",
-    boxShadow: '0 0 23px 4px black',
+    color: "#6d0000",
+    boxShadow: '0 0 10px 0.03px black',
     
 }
 
 const imageStyle: CSSProperties = {
-    maxWidth: "8rem",
-    borderRadius: "1rem",
+    maxWidth: "20rem",
     margin: ".5rem 0",
+    borderRadius: '.02rem'
 }
 
 const pStyling: CSSProperties = {
     fontSize: '0.7rem',
-    textAlign: 'left',
-    paddingLeft: '2rem',
-    width: '5rem'
 }
 
 const innerDivStyle: CSSProperties = {
@@ -79,13 +76,19 @@ const innerDivStyle: CSSProperties = {
     position: 'relative',
     justifyContent: 'center',
     alignItems: 'center',
-    flexDirection: 'row'
+    flexDirection: 'column'
 }
 
 const paraStyle: CSSProperties = {
-    fontSize: '0.8rem',
+    fontSize: '0.9rem',
+    fontWeight: 'normal',
     width: '80%',
-    textAlign: 'left',
     paddingLeft: '2rem'
+}
+
+const h2Style: CSSProperties = {
+    fontSize: '2rem',
+    fontFamily: 'Shadows Into Light',
+    margin: '0rem 0rem 0rem 0rem'
 }
 export default DetailView;
