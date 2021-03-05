305bfce276cb650820f2c7b95a5eb6b6eb045ff1
Camilla Johansson <camilla.johansson1986@gmail.com>
2021-03-05 17:13:36 +0100

    invalid style fix

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,52 +1,64 @@
-import React, { ChangeEvent, Component, CSSProperties } from 'react';
-interface Props {
-   value: string;
-   onChange: (value: string) => void;
-   dropDownList: any[];
+import React, { ChangeEvent, Component, CSSProperties} from "react";
+interface Props{
+    value: string;
+    onChange: (value: string) => void;
+    dropDownList: any[];
 }
 interface State {}
-class SearchBar extends Component<Props, State> {
-   handleChange = (event: ChangeEvent<HTMLInputElement>) => {
-      this.props.onChange(event.target.value);
-   };
+class SearchBar extends Component<Props,State>  {
 
-   render() {
-      const dropDownList = this.props.dropDownList;
-      return (
-         <div>
-            <input
-               list="listid"
-               style={searchBox}
-               placeholder="Drink"
-               value={this.props.value}
-               onChange={this.handleChange}
-            />
-            <datalist id="listid" style={dropDownListStyle}>
-               {dropDownList.map((list, i) => (
-                  <option key={i} label={list.strDrink} value={list.strDrink} />
-               ))}
-            </datalist>
-         </div>
-      );
-   }
+    handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+        this.props.onChange(event.target.value)
+    }
+
+    render() {
+        const dropDownList = this.props.dropDownList;
+        return (
+            <div>
+                <input 
+                    list="listid"
+                    style={searchBox}
+                    placeholder="Drink"
+                    value={this.props.value}
+                    onChange={this.handleChange}
+                />
+                <datalist id='listid' style={dropDownListStyle}>
+                    {dropDownList ?  dropDownList.map((list, i) => (
+                        <option 
+                            key={i}
+                            label={list.strDrink} 
+                            value={list.strDrink}
+                        />
+                    )): []}
+                   
+                </datalist>
+            </div>
+        );
+    }
 }
 
 const searchBox: CSSProperties = {
-   maxWidth: '12rem',
-   height: '1.5rem',
-   background: 'white',
-   padding: '.5rem',
-   fontSize: '1.5rem',
-   border: 'none',
-   borderRadius: '.5rem',
-   marginBottom: '2rem',
-};
+    width: '16rem',
+    height: '1.5rem',
+    background: 'white',
+    padding: '.5rem',
+    fontSize: '1.5rem',
+    border: 'none',
+    borderRadius: '.5rem',
+    backgroundImage:'none',
+    boxShadow: 'none',
+    marginBottom: '2rem',
+    outline: 'none'
+}
 
 const dropDownListStyle: CSSProperties = {
-   position: 'fixed',
-   border: 'none',
-   borderRadius: '.5rem',
-   marginBottom: '2rem',
-};
+    position: 'absolute',
+    border: 'none',
+    borderRadius: '.5rem',
+    backgroundImage:'none',
+    boxShadow: 'none',
+    marginBottom: '2rem'
+}
+
 
 export default SearchBar;
